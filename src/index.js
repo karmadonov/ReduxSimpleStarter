1cec2a4fb9d7a57288e6e1b4bc149721a41ca27a
Kirill Karmadonov <kirill@live.com>
2018-07-22 21:58:22 +0300

    Export Statements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+
 const API_KEY = 'AIzaSyAqLWjQtyi-k_Ueaa0ImYSb5_roU5iZRJy';
 
 // Create a new component. This component should produce
 // some HTML
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it

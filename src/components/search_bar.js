2e30850e4d1f0cc8cd38bfb42e2082e26088970b
Kirill Karmadonov <kirill@live.com>
2018-07-23 18:46:35 +0300

    Handling User Events

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,8 +4,14 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
 
     render() {
-        return <input/>;
+        return <input onChange={
+            event => console.log(event.target.value)
+        } />;
     };
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
 
 };
 

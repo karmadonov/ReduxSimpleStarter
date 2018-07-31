ab27963cc309bbefb861d668b4320ae86adca174
Kirill Karmadonov <kirill@live.com>
2018-07-31 17:34:15 +0300

    Props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 
 const API_KEY = 'AIzaSyAqLWjQtyi-k_Ueaa0ImYSb5_roU5iZRJY';
@@ -31,6 +32,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={ this.state.videos }/>
             </div>
         );
     }
